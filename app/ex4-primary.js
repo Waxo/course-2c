aaa922ba7e43243236ac645378c2756f954edbfc
Maxime Robin <contact.waxo@gmail.com>
2017-03-10 14:48:19 +0100

    Added ex4, missing test if no number in string

---
+++ app/ex4-primary.js
@@ -0,0 +1,22 @@
+String.prototype.containsNumber = function (number) {
+  return this.indexOf(number) >= 0;
+};
+
+String.prototype.hasNumber = function () {
+  return /[0-9]/.test(this);
+};
+
+String.prototype.toNumber = function () {
+  // /[^0-9]/g <=> new RegExp('[^0-9]', 'g');
+  return Number(this.replace(/[^0-9]/g, ''));
+};
+
+// console.log('Contains number');
+// let ret = 'aze12qsd13wxc14'.containsNumber(12);
+// console.log(ret);
+//
+// console.log('hasNumber');
+// console.log('aze12qsd13wxc14'.hasNumber());
+//
+// console.log('toNumber');
+// console.log('aze12qsd13wxc14'.toNumber());
